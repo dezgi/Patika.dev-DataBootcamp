f6ca726403802f443830a28812a3f363a1a83404
Ezgi Dişbudak <86930319+dezgi@users.noreply.github.com>
2025-05-17 00:16:15 +0300

    Add files via upload
    Titanic veri seti altında aşağıdaki soruları, notebook açarak çözebilirsiniz. Her sorunun markdown olarak yazılması ve altına da kodunun yazılması daha okunaklı kılar kodlarınızı.
    Kazada ölenlerin yaş ortalamasını bulunuz
    Kazada ölenlerin bilet fiyatlarının ortalamasını ve medyanını bulunuz
    Kazada ölen erkeklerin yaş ortalamasını bulunuz
    Kazada ölen Kadınların yaş ortalamasını bulunuz
    
    Kazadan kurtulanların yaş ortalamasını bulunuz
    Kazadan kurtulanların bilet fiyatlarının ortalamasını bulunuz
    Kazadan kurtulan toplam kişi sayısını bulunuz… 
    (tüyo: len(df_titanic[(df_titanic.Survived==1)]   kodu ile de bu sayıya ulaşabilirsiniz.)
    
    10 yaşından küçüklerin bilet fiyatlarının medyan değerini bulunuz
    
    1.sınıf, 2.sınıf ve 3.sınıf bilet fiyatlarının ortalama ve medyanlarını karşılaştırınız. (Pclass değişkeni sınıfları barındırmaktadır.)
    
    Kazada ölen kadınların oranı ile erkeklerin oranını karşılaştırınız. (Örnek: erkekler için; ölen erkeklerin, erkek sayısına bölümü bu oranı vermektedir.

---
+++ PandasHomework.py
@@ -0,0 +1,59 @@
+import pandas as pd
+import numpy as np
+
+#csv yükleme ve birkaç satırı okuma
+
+dataset = pd.read_csv("C:\\Users\\ezgi.disbudak\\Desktop\\Ezgi\\PatikaDevDataEğitim\\Week3\\Titanic_dataset.csv")
+dataset.head()
+
+# 1. Kazada ölenlerin yaş ortalamasını bulunuz
+result = dataset[dataset['Survived'] == 0]['Age'].mean()
+print(result)
+
+# 2. Kazada ölenlerin bilet fiyatlarının ortalamasını ve medyanını bulunuz
+mean = dataset[dataset['Survived'] == 0]['Fare'].mean()
+median = dataset[dataset['Survived'] == 0]['Fare'].median()
+
+print(mean)
+print(median)
+
+# 3. Kazada ölen erkeklerin yaş ortalamasını bulunuz
+result = dataset[(dataset['Survived'] == 0) & (dataset['Sex'] == 'male')]['Age'].mean()
+print(result)
+
+# 4. Kazada ölen Kadınların yaş ortalamasını bulunuz
+result = dataset[(dataset['Survived'] == 0) & (dataset['Sex'] == 'female')]['Age'].mean()
+print(result)
+
+# 5. Kazadan kurtulanların yaş ortalamasını bulunuz
+result = dataset[dataset['Survived'] == 1]['Age'].mean()
+print(result)
+
+# 6. Kazadan kurtulanların bilet fiyatlarının ortalamasını bulunuz
+result = dataset[dataset['Survived'] == 1]['Fare'].mean()
+print(result)
+
+# 7. Kazadan kurtulan toplam kişi sayısını bulunuz… 
+result = len(dataset[dataset['Survived'] == 1])
+print(result)
+
+# 8. 10 yaşından küçüklerin bilet fiyatlarının medyan değerini bulunuz
+result = dataset[dataset['Age'] < 10]['Fare'].median()
+print(result)
+
+# 9. 1.sınıf, 2.sınıf ve 3.sınıf bilet fiyatlarının ortalama ve medyanlarını karşılaştırınız. (Pclass değişkeni sınıfları barındırmaktadır.)
+dataset.groupby("Pclass")["Fare"].agg(["mean", "median"])
+
+# 10. Kazada ölen kadınların oranı ile erkeklerin oranını karşılaştırınız. 
+#(Örnek: erkekler için; ölen erkeklerin, erkek sayısına bölümü bu oranı vermektedir.
+
+numberOfMale = len(dataset[dataset['Sex'] == 'male'])
+numberOfDeadMale = len(dataset[(dataset['Survived'] == 0) & (dataset['Sex'] == 'male')])
+ratioMale = numberOfDeadMale / numberOfMale
+
+numberOfFemale = len(dataset[dataset['Sex'] == 'female'])
+numberOfDeadFemale = len(dataset[(dataset['Survived'] == 0) & (dataset['Sex'] == 'female')])
+ratioFemale = numberOfDeadFemale / numberOfFemale
+
+print("Ratio male:", ratioMale * 100)
+print("Ratio female:", ratioFemale * 100)
